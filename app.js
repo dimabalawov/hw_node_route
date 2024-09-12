9bea8664ea04861be7df63a5cf770af0b0e97e23
dimabalawov <dimabalawov@gmail.com>
2024-09-12 14:01:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+
+// Обработка формы Входа
+app.post('/login', (req, res) => {
+    const login = req.body.login;
+    const password = req.body.password;
+    const logData = `Login: ${login}, Password: ${password}\n`;
+
+    // Записываем данные в файл
+    fs.appendFile('login_attempts.txt', logData, (err) => {
+        if (err) throw err;
+        console.log('Данные входа записаны.');
+    });
+
+    res.send('Попытка входа записана.');
+});
+
+// Обработка формы Регистрации
+app.post('/register', (req, res) => {
+    const { login, password, confirmPassword, email } = req.body;
+    const regData = `Login: ${login}, Password: ${password}, Email: ${email}\n`;
+
+    // Записываем данные в файл
+    fs.appendFile('registration_data.txt', regData, (err) => {
+        if (err) throw err;
+        console.log('Данные регистрации записаны.');
+    });
+
+    res.send('Регистрация завершена.');
+});
+
+// Запуск сервера
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
+});
